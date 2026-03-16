96f3bc3698d67f69f79f218e48ee66bf1e64724a
teofanaenachioiu <teofanaenachioiu@gmail.com>
2026-03-16 13:01:06 +0200

    Initialize Spring Boot project

---
+++ java/src/main/java/org/ikigaidigital/TimeDepositApplication.java
@@ -0,0 +1,13 @@
+package org.ikigaidigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeDepositApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeDepositApplication.class, args);
+    }
+
+}
---
+++ java/src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE time_deposits (
+                            id INTEGER PRIMARY KEY,
+                            plan_type VARCHAR(50) NOT NULL,
+                            days INTEGER NOT NULL,
+                            balance DECIMAL(15,2) NOT NULL
+);
+
+CREATE TABLE withdrawals (
+                            id SERIAL PRIMARY KEY,
+                            time_deposit_id INTEGER NOT NULL,
+                            amount DECIMAL(15,2) NOT NULL,
+                            transaction_date DATE NOT NULL,
+
+                            CONSTRAINT fk_deposit
+                                FOREIGN KEY(time_deposit_id)
+                                REFERENCES time_deposits(id)
+);
